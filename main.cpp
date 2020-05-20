c98310fb6f3126110b3493b2461540c7b8412d8f
Sebastien Blazej <sebik.blazej@gmail.com>
2020-05-20 17:32:45 -0400

    Refactoring and rendering changes.
    * Rendering now uses hardware accelerated renderer instead of screen surfaces.
    * Code encapsulated into larger functions for game initialization, looping, and closing.
    + Added some print statements to better follow errors and program flow.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
 #include <stdio.h>
 #include "SDL.h"
-#include "SDL_image.h"
-
-
-// Global Constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 360;
+#include "constants.h"
 
 class Entity {
 private:
@@ -19,61 +14,102 @@ public:
 	Entity(float x, float y) {
 		this->posX = x;
 		this->posY = y;
-		this->sizeX = 16.0;
-		this->sizeY = 16.0;
+		this->sizeX = BLOCKSIZE;
+		this->sizeY = BLOCKSIZE;
 	}
 };
 
+SDL_Window* windowMain = NULL;
 
-int main(int argc, char* args[]) {
-	// Initialize SDL
+SDL_Renderer* rendererMain = NULL;
+
+
+int gameInit() {
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		printf("ERROR: SDL could not initialize: %s\n", SDL_GetError());
 		return -1;
 	}
-
-	// The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
-
-	// Create the window we will render to
-	SDL_Window* mainWindow = SDL_CreateWindow("SMB3DO", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-	if (mainWindow == NULL) {
-		printf("ERROR: Window could not be created: %s\n", SDL_GetError());
+	windowMain = SDL_CreateWindow("SMB3DO", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+	if (windowMain == NULL) {
+		printf("ERROR: Main window could not be created: %s\n", SDL_GetError());
 		return -1;
 	}
+	rendererMain = SDL_CreateRenderer(windowMain, -1, SDL_RENDERER_ACCELERATED);
+	if (rendererMain == NULL) {
+		printf("ERROR: Main renderer could not be created: %s\n", SDL_GetError());
+		return -1;
+	}
+	return 0;
+}
 
-	// Misc variables
-	bool quit = false;
+int gameLoop() {
+	bool quitGame = false;
+	SDL_Event e;
+
 	Entity mario = Entity(0, 0);
 
-	// Game loop
-	SDL_Event e;
-	while ( !quit ) {
-		// Handle all events
-		while ( SDL_PollEvent(&e) != 0 ) {
-			// User requests to quit the program
-			if (e.type == SDL_QUIT) {
-				quit = true;
+	while (!quitGame) {
+		// Handle events
+		while (SDL_PollEvent(&e) != 0) {
+			if (e.type == SDL_QUIT) quitGame = true;
+			if (e.type == SDL_KEYDOWN) {
+				if (e.key.keysym.scancode == SDL_SCANCODE_ESCAPE) quitGame = true;
 			}
 		}
+
 		// Handle keyboard input
 		const Uint8* keyStateCurrent = SDL_GetKeyboardState(NULL);
-		if ( keyStateCurrent[SDL_SCANCODE_W] ) { mario.posY += -0.05; }
-		if ( keyStateCurrent[SDL_SCANCODE_D] ) { mario.posX += 0.05; }
-		if ( keyStateCurrent[SDL_SCANCODE_S] ) { mario.posY += 0.05; }
-		if ( keyStateCurrent[SDL_SCANCODE_A] ) { mario.posX += -0.05; }
+		if (keyStateCurrent[SDL_SCANCODE_W]) mario.posY += -0.05 * SCREEN_SCALE;
+		if (keyStateCurrent[SDL_SCANCODE_D]) mario.posX += 0.05 * SCREEN_SCALE;
+		if (keyStateCurrent[SDL_SCANCODE_S]) mario.posY += 0.05 * SCREEN_SCALE;
+		if (keyStateCurrent[SDL_SCANCODE_A]) mario.posX += -0.05 * SCREEN_SCALE;
 
-		// Get window surface
-		screenSurface = SDL_GetWindowSurface(mainWindow);
-
-		// Fill the surface with white color
-		SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0x00));
+		// Fill the surface with black color and the the SNES 'playscreen' with white color
+		SDL_SetRenderDrawColor(rendererMain, 0x00, 0x00, 0x00, 0x00);
+		SDL_RenderFillRect(rendererMain, NULL);
+		int playScreenPosX = (WINDOW_WIDTH - SCREEN_WIDTH) / 2;
+		int playScreenPosY = (WINDOW_HEIGHT - SCREEN_HEIGHT) / 2;
+		SDL_Rect playScreen = { playScreenPosX, playScreenPosY, SCREEN_WIDTH, SCREEN_HEIGHT };
+		SDL_SetRenderDrawColor(rendererMain, 0x10, 0x10, 0x40, 0x00);
+		SDL_RenderFillRect(rendererMain, &playScreen);
 
 		// Color in the player
 		SDL_Rect playerRect = { mario.posX, mario.posY, mario.sizeX, mario.sizeY };
-		SDL_FillRect(screenSurface, &playerRect, SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0xF0));
+		SDL_SetRenderDrawColor(rendererMain, 0x00, 0x00, 0xC0, 0x00);
+		SDL_RenderFillRect(rendererMain, &playerRect);
 
 		// Update the surface
-		SDL_UpdateWindowSurface(mainWindow);
+		SDL_RenderPresent(rendererMain);
 	}
+
+	return 0;
+}
+
+int gameClose() {
+	SDL_DestroyRenderer(rendererMain);
+	SDL_DestroyWindow(windowMain);
+	SDL_Quit();
+
+	return 0;
+}
+
+
+int main(int argc, char* args[]) {
+	printf("Initializng game...\n");
+	if (gameInit() < 0) {
+		printf("ERROR: Failed to initialize game\n");
+		return -1;
+	}
+	printf("Starting main game loop...\n");
+	if (gameLoop() < 0) {
+		printf("ERROR: Game loop exited early\n");
+		return -1;
+	}
+	printf("Closing game...\n");
+	if (gameClose() < 0) {
+		printf("ERROR: Failed to deallocate resources properly and shutdown SDL\n");
+		return -1;
+	}
+
+	return 0;
 }
