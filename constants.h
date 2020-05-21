0b9c0dc3cad9a0720620e44f44e6d2901cf3a171
Sebastien Blazej <sebik.blazej@gmail.com>
2020-05-21 01:12:15 -0400

    Dynamic scaling constants
    + Added debug constant
    * Scaling factor is now per-axis rather than universal
    * Scaled 'screen' is now 4:3 instead of 1:1
    * Fixed instances of 'SNES' when it should be 'NES'

--- constants.h
+++ constants.h
@@ -1,16 +1,21 @@
 #pragma once
 #include <math.h>
 
+
+// Debugging
+const bool DEBUG = true;
+
 // Window attributes
-const short WINDOW_WIDTH = 512;				// \ Actual output resolution
-const short WINDOW_HEIGHT = 448;			// / Should be 4:3 or 16:9 resolutions
+const short WINDOW_WIDTH = 1080;  // \ Actual output resolution
+const short WINDOW_HEIGHT = 640;  // / Should be 4:3 or 16:9 resolutions
 
-// SNES dimensions
-const short WINDOW_WIDTH_SNES = 256;		// \ Original SNES's typical output resolution.
-const short WINDOW_HEIGHT_SNES = 224;		// / Note its 8:7 aspect ratio.
+// NES dimensions
+const short WINDOW_WIDTH_NES = 256;	  // \ Original NES's typical output resolution.
+const short WINDOW_HEIGHT_NES = 224;  // / Note its 8:7 aspect ratio.
+const short BLOCKSIZE_NES = 16;
 
-
-const unsigned char SCREEN_SCALE = 1 << static_cast<int>(floor(log2(WINDOW_HEIGHT / WINDOW_HEIGHT_SNES)));
-const short SCREEN_WIDTH = WINDOW_WIDTH_SNES * SCREEN_SCALE;		// \ Size of the 8:7 'play-screen'
-const short SCREEN_HEIGHT = WINDOW_HEIGHT_SNES * SCREEN_SCALE;		// /
-const short BLOCKSIZE = 16 * SCREEN_SCALE;		// The output size of a SNES 16x16 block
+// Scaling for the NES to fit output window, but stretched to 4:3
+// TODO: Fix for output resolutions smaller than 4:3 by adapting horizontal size first instead of vertical
+// TODO: Make screen scaling realtime to window change; will probably require moving to a .cpp files
+const double SCREEN_SCALE_X = floor(WINDOW_HEIGHT / (WINDOW_HEIGHT_NES * 6.0/7.0));  // We first find the integer upscale such that the screen takes the most space vertically
+const double SCREEN_SCALE_Y = SCREEN_SCALE_X * 6.0/7.0;  // The Y-axis is further scaled to make the screen 4:3
