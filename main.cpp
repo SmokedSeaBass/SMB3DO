c668887b4f231f6bf1b4b5e375b1545459d6ba2d
Sebastien Blazej <sebik.blazej@gmail.com>
2020-05-21 01:22:34 -0400

    Resolution management.
    + Added viewport to main renderer
    + Added render scale to main renderer
    * All rendering should now be done on a 256x224 canvas

--- main.cpp
+++ main.cpp
@@ -14,14 +14,19 @@ public:
 	Entity(float x, float y) {
 		this->posX = x;
 		this->posY = y;
-		this->sizeX = BLOCKSIZE;
-		this->sizeY = BLOCKSIZE;
+		this->sizeX = BLOCKSIZE_NES;
+		this->sizeY = BLOCKSIZE_NES;
 	}
 };
 
 SDL_Window* windowMain = NULL;
 
 SDL_Renderer* rendererMain = NULL;
+
+const SDL_Rect screenRect = { (WINDOW_WIDTH - round(WINDOW_WIDTH_NES * SCREEN_SCALE_X)) / 2,
+	(WINDOW_HEIGHT - round(WINDOW_HEIGHT_NES * SCREEN_SCALE_Y)) / 2,
+	round(WINDOW_WIDTH_NES * SCREEN_SCALE_X),
+	round(WINDOW_HEIGHT_NES * SCREEN_SCALE_Y) };
 
 
 int gameInit() {
@@ -39,6 +44,10 @@ int gameInit() {
 		printf("ERROR: Main renderer could not be created: %s\n", SDL_GetError());
 		return -1;
 	}
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, 0);  // Use nearest pixel scaling
+	SDL_RenderSetViewport(rendererMain, &screenRect);  // This should be the last rendering operation done UNSCALED; the coordinates and rect are real-sized
+	printf("Viewport Absolute Dimensions: %i x %i\n", screenRect.w, screenRect.h);
+	SDL_RenderSetScale(rendererMain, SCREEN_SCALE_X, SCREEN_SCALE_Y);  // All drawing isss now scaled from a 256x224 canvas
 	return 0;
 }
 
@@ -59,17 +68,13 @@ int gameLoop() {
 
 		// Handle keyboard input
 		const Uint8* keyStateCurrent = SDL_GetKeyboardState(NULL);
-		if (keyStateCurrent[SDL_SCANCODE_W]) mario.posY += -0.05 * SCREEN_SCALE;
-		if (keyStateCurrent[SDL_SCANCODE_D]) mario.posX += 0.05 * SCREEN_SCALE;
-		if (keyStateCurrent[SDL_SCANCODE_S]) mario.posY += 0.05 * SCREEN_SCALE;
-		if (keyStateCurrent[SDL_SCANCODE_A]) mario.posX += -0.05 * SCREEN_SCALE;
+		if (keyStateCurrent[SDL_SCANCODE_W]) mario.posY += -0.05;
+		if (keyStateCurrent[SDL_SCANCODE_D]) mario.posX += 0.05;
+		if (keyStateCurrent[SDL_SCANCODE_S]) mario.posY += 0.05;
+		if (keyStateCurrent[SDL_SCANCODE_A]) mario.posX += -0.05;
 
-		// Fill the surface with black color and the the SNES 'playscreen' with white color
-		SDL_SetRenderDrawColor(rendererMain, 0x00, 0x00, 0x00, 0x00);
-		SDL_RenderFillRect(rendererMain, NULL);
-		int playScreenPosX = (WINDOW_WIDTH - SCREEN_WIDTH) / 2;
-		int playScreenPosY = (WINDOW_HEIGHT - SCREEN_HEIGHT) / 2;
-		SDL_Rect playScreen = { playScreenPosX, playScreenPosY, SCREEN_WIDTH, SCREEN_HEIGHT };
+		// Fill the NES 'screen' with blue color
+		SDL_Rect playScreen = { 0, 0, WINDOW_WIDTH_NES, WINDOW_HEIGHT_NES };
 		SDL_SetRenderDrawColor(rendererMain, 0x10, 0x10, 0x40, 0x00);
 		SDL_RenderFillRect(rendererMain, &playScreen);
 
@@ -89,7 +94,6 @@ int gameClose() {
 	SDL_DestroyRenderer(rendererMain);
 	SDL_DestroyWindow(windowMain);
 	SDL_Quit();
-
 	return 0;
 }
 
@@ -100,11 +104,13 @@ int main(int argc, char* args[]) {
 		printf("ERROR: Failed to initialize game\n");
 		return -1;
 	}
+
 	printf("Starting main game loop...\n");
 	if (gameLoop() < 0) {
 		printf("ERROR: Game loop exited early\n");
 		return -1;
 	}
+
 	printf("Closing game...\n");
 	if (gameClose() < 0) {
 		printf("ERROR: Failed to deallocate resources properly and shutdown SDL\n");
