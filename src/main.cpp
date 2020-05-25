05aa20fcbf8ed271fa5f25266c8b343120dc5e00
Sebastien Blazej <sebik.blazej@gmail.com>
2020-05-25 19:43:01 -0400

    Implement changed includes and function names

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,23 @@
 #include <stdio.h>
-#include "SDL.h"
-#include "constants.h"
+#include <SDL.h>
 #include "game.h"
 
 int main(int argc, char* args[]) {
-	printf(ANSI_COLOR_CYAN "Initializng game...\n" ANSI_COLOR_RESET);
+	printf("Initializng game...\n");
 	Game smb3do;
-	if (smb3do.init() < 0) {
+	if (smb3do.Initialize() < 0) {
 		printf("ERROR: Failed to initialize game\n");
 		return -1;
 	}
 
 	printf("Starting main game loop...\n");
-	if (smb3do.run() < 0) {
+	if (smb3do.Run() < 0) {
 		printf("ERROR: Game was terminated early\n");
 		return -1;
 	}
 
 	printf("Closing game...\n");
-	smb3do.close();
+	smb3do.Close();
 
 	return 0;
 }
