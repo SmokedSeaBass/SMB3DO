28c4afbb9e86297d8dbcda0da942be55b097d368
Sebastien Blazej <sebik.blazej@gmail.com>
2020-05-30 17:31:02 -0400

    Bitmap tile rendering
    + Preliminary documentation
    * Tilemaps are now maps of Tile pointers instead of unsigned chars

--- src/tilemap.cpp
+++ src/tilemap.cpp
@@ -4,58 +4,47 @@
 #include <stdexcept>
 #include "constants.h"
 
-Tilemap::Tilemap(unsigned char map[], unsigned int width, unsigned int height) {
+Tilemap::Tilemap(Tile* map[], unsigned int width, unsigned int height) {
 	width_ = width;
 	height_ = height;
-	map_ = new unsigned char[width_ * height_];
-	memcpy(map_, map, sizeof(char) * (width_ * height_));
+	map_ = new Tile*[width_ * height_];
+	memcpy(map_, map, sizeof(Tile*) * (width_ * height_));
 }
 
 Tilemap::~Tilemap() {
 	delete[] map_;
 }
 
+
 void Tilemap::GetDimensions(int dimensions[]) {
 	dimensions[0] = width_;
 	dimensions[1] = height_;
 }
 
-unsigned char Tilemap::GetTileId(int x, int y) {
+Tile* Tilemap::GetTile(int x, int y) {
 	if (x >= width_ || y >= height_) {
 		throw std::out_of_range("ERROR: Attempted to get tile outside of tilemap");
 	}
 	return map_[y * width_ + x];
 }
 
-unsigned char Tilemap::SetTileId(int x, int y, unsigned char tile_id) {
+void Tilemap::SetTile(int x, int y, Tile* tile) {
 	if (x >= width_ || y >= height_) {
 		throw std::out_of_range("ERROR: Attempted to set tile outside of tilemap");
 	}
-	map_[y * width_ + x] = tile_id;
-	return map_[y * width_ + x];
+	map_[y * width_ + x] = tile;
 }
 
 int Tilemap::Draw(Graphics& graphics, int pos_x, int pos_y) {
-	SDL_Rect block_rect = { 0, 0, BLOCKSIZE_NES, BLOCKSIZE_NES };
-	SDL_Color color = { 0x00, 0x00, 0x00, 0xFF };
+	Tile* tile = nullptr;
 	// TODO: Optimize rendering such that only visible tiles are drawn
 	for (int y = 0; y < height_; y++) {
-		block_rect.y = pos_y + y * BLOCKSIZE_NES;
 		for (int x = 0; x < width_; x++) {
-			block_rect.x = pos_x + x * BLOCKSIZE_NES;
-			switch (map_[y * width_ + x]) {
-			case 0x00:
-				continue;
-			case 0x01:
-				color = SDL_COLOR_WHITE;
-				break;
-			case 0x02:
-				color = SDL_COLOR_BLACK;
-				break;
-			default:
+			tile = map_[y * width_ + x];
+			if (tile == nullptr) {
 				continue;
 			}
-			graphics.BlitColoredRect(&block_rect, color.r, color.g, color.b, color.a);
+			tile->Draw(graphics, x * BLOCKSIZE_NES + pos_x, y * BLOCKSIZE_NES + pos_y);
 		}
 	}
 	return 0;
--- src/tilemap.h
+++ src/tilemap.h
@@ -1,19 +1,27 @@
 #pragma once
 #include <vector>
 #include "graphics.h"
+#include "tile.h"
 
+/**
+*  \brief Handles the tile grid routines.
+* 
+*  \description A Tilemap is a data structure that organizes tiles into a 2-D grid.  Each entry in the tilemap's map is
+*               an unsigned char corresponding to a unique tile, which has it's own properties, interaction routine, and drawing routine.
+*               These properties are defined in the Tile class.
+*/
 class Tilemap {
 public:
-	Tilemap(unsigned char map[], unsigned int width, unsigned int height);
+	Tilemap(Tile* map[], unsigned int width, unsigned int height);
 	~Tilemap();
 
 	void GetDimensions(int dimensions[]);
-	unsigned char GetTileId(int x, int y);
-	unsigned char SetTileId(int x, int y, unsigned char tile_id);
+	Tile* GetTile(int x, int y);
+	void SetTile(int x, int y, Tile* tile);
 	int Draw(Graphics& graphics, int pos_x, int pos_y);
 
 private:
 	unsigned int width_;
 	unsigned int height_;
-	unsigned char* map_;
+	Tile** map_;
 };
