b9e7252f2c7c8fe4331cb2647f6f7a38b92bfd9d
Sebastien Blazej <sebik.blazej@gmail.com>
2020-05-30 17:17:16 -0400

    Documentation and test new features.
    + Added preliminary documentation
    * Moved code from pointless methods to constructors/decontructors
    + Added code to test new tileset/tilemap features

--- src/game.cpp
+++ src/game.cpp
@@ -10,15 +10,14 @@ Game::Game() {
 Game::~Game() {
 }
 
-int Game::Initialize() {
-	if (graphics_.Initialize() < 0) {
-		return -1;
-	} 
-	return 0;
-}
-
 int Game::Run() {
+	Graphics graphics;
+	if (graphics.Initialize() < 0) {
+		printf("ERROR: Grpahics could not initialize: %s\n", SDL_GetError());
+		return -1;
+	}
 
+	// to be moved to its own class
 	class Entity {
 	private:
 
@@ -35,59 +34,61 @@ int Game::Run() {
 			this->size_y_ = BLOCKSIZE_NES;
 		}
 	};
-	Entity mario = Entity(0, 0);
-	Sprite mario_spr = Sprite(graphics_, "D:/Users/Sebik/Documents/Code/Visual Studio/smb3do/content/tilesets/master.bmp", 392, 86, BLOCKSIZE_NES, BLOCKSIZE_NES);
+	Entity mario = Entity(WINDOW_WIDTH_NES/2.0, WINDOW_HEIGHT_NES/2.0);
+	Sprite mario_spr = Sprite(graphics, "content/tilesets/debug.bmp", 0, 0, 16, 0, BLOCKSIZE_NES, BLOCKSIZE_NES);
 
-	unsigned char background_tiles[25] = {
-		0x01, 0x01, 0x01, 0x01, 0x01,
-		0x01, 0x02, 0x01, 0x02, 0x01,
-		0x01, 0x01, 0x01, 0x01, 0x01,
-		0x00, 0x01, 0x01, 0x01, 0x00,
-		0x01, 0x00, 0x00, 0x00, 0x01
+	Sprite tileset_debug = Sprite(graphics, "content/tilesets/debug.bmp");
+	Tile air = Tile(0x00, &tileset_debug, { 0, 0, 16, 16 }, "air");
+	Tile wood = Tile(0x01, &tileset_debug, { 0, 16, 16, 16 }, "wood");
+	
+
+	Tile* background_tiles[30] = {
+		nullptr, &air, & air, &air, nullptr,
+		&air, &wood, & air, &wood, &air,
+		&air, &air, & air, &air, &air,
+		&wood, &air, & air, &air, &wood,
+		&air, &wood, & wood, &wood, &air,
+		nullptr, &air, & air, &air, nullptr
 	};
-	Tilemap background_tilemap = Tilemap(background_tiles, 5, 5);
+	Tilemap background_tilemap = Tilemap(background_tiles, 5, 6);
 
 	bool quitGame = false;
 	SDL_Event event;
 	const Uint8* keyStateCurrent = SDL_GetKeyboardState(NULL);
 
 	while (!quitGame) {
+		/* Get Input */
 		// Handle events
 		while (SDL_PollEvent(&event) != 0) {
 			if (event.type == SDL_QUIT) quitGame = true;
 			if (event.type == SDL_KEYDOWN) {
 				if (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE) quitGame = true;
-				if (event.key.keysym.scancode == SDL_SCANCODE_F11) graphics_.WindowToggleFullscreen();
+				if (event.key.keysym.scancode == SDL_SCANCODE_F11) graphics.WindowToggleFullscreen();
 			}
 		}
-
+		
+		/* Update */
 		// Handle keyboard input
-		if (keyStateCurrent[SDL_SCANCODE_W]) mario.pos_y_ += -0.5f;
-		if (keyStateCurrent[SDL_SCANCODE_D]) mario.pos_x_ += 0.5f;
-		if (keyStateCurrent[SDL_SCANCODE_S]) mario.pos_y_ += 0.5f;
-		if (keyStateCurrent[SDL_SCANCODE_A]) mario.pos_x_ += -0.5f;
+		if (keyStateCurrent[SDL_SCANCODE_W]) mario.pos_y_ += -1.0f;
+		if (keyStateCurrent[SDL_SCANCODE_D]) mario.pos_x_ += 1.0f;
+		if (keyStateCurrent[SDL_SCANCODE_S]) mario.pos_y_ += 1.0f;
+		if (keyStateCurrent[SDL_SCANCODE_A]) mario.pos_x_ += -1.0f;
 
+		/* Draw */
 		// Fill the NES 'screen' with blue color
-		graphics_.BlitColoredRect(NULL, 0x10, 0x10, 0x40, 0x7F);
-
+		graphics.BlitColoredRect(NULL, 0x10, 0x10, 0x40, 0xFF);
 		// Display tilemap
-		background_tilemap.Draw(graphics_, 16, 16);
-
+		background_tilemap.Draw(graphics, 16, 16);
 		// Color in the player
 		SDL_Rect playerRect = { mario.pos_x_, mario.pos_y_, mario.size_x_, mario.size_y_ };
-		graphics_.BlitColoredRect(&playerRect, 0xC0, 0x00, 0xC0, 0xFF);
-		mario_spr.Draw(graphics_, playerRect.x, playerRect.y);
-		mario_spr.Draw(graphics_, playerRect.x + 16, playerRect.y);
-		mario_spr.Draw(graphics_, playerRect.x, playerRect.y + 16);
+		//graphics.BlitColoredRect(&playerRect, 0xC0, 0x00, 0xC0, 0xFF);
+		mario_spr.Draw(graphics, playerRect.x, playerRect.y, { 16, 0, 16, 16 });
+		mario_spr.SetRect(32, 0, 16, 16);
+		mario_spr.Draw(graphics, playerRect.x + 16, playerRect.y);
+		mario_spr.Draw(graphics, playerRect.x, playerRect.y + 16);
 
 		// Flip to screen
-		graphics_.FlipRenderer();
+		graphics.FlipRenderer();
 	}
-
 	return 0;
-}
-
-void Game::Close() {
-	graphics_.Close();
-	SDL_Quit();
 }
--- src/game.h
+++ src/game.h
@@ -1,15 +1,20 @@
 #pragma once
 #include "graphics.h"
 
+/**
+ *  \file game.h
+ *
+ *  Include file for the Game class
+ */
+
 class Game {
 public:
 	Game();
 	~Game();
 
-	int Initialize();
+	/// <summary>
+	/// Runs the game's main logical loop.
+	/// </summary>
+	/// <returns>0 on game loop end, -1 if the loop was terminated early</returns>
 	int Run();
-	void Close();
-
-private:
-	Graphics graphics_;
 };
