538cabfa9ca8bd14bbfcb0015eb794fd24b803d1
Sebastien Blazej <sebik.blazej@gmail.com>
2020-05-30 17:27:26 -0400

    Transparency and custom rect rendering.
    + Sprites can now have one RGB value be treated as transparent when rendering
    + Sprite rendering rectangle can now be set/get
    + Sprite rendering can use a custom source rect, bypassing the sprite's internal rect
    + Sprites can now be render to a custom rect region, instead of being locked to the internal rect's dimensions
    + Added preliminary documentation
    * Sprites can effectively function as tilesets

--- src/sprite.cpp
+++ src/sprite.cpp
@@ -1,25 +1,74 @@
 #include "sprite.h"
 
-Sprite::Sprite(Graphics& graphics, const std::string& file_path, int source_x, int source_y, int width, int height) {
-	source_rect_ = { source_x, source_y, width, height };
+Sprite::Sprite(Graphics& graphics, const std::string& file_path, int alpha_x, int alpha_y, int source_x, int source_y, int source_w, int source_h) {
+	source_rect_ = { source_x, source_y, source_w, source_h };
 	SDL_Surface* sprite_surface = SDL_LoadBMP(file_path.c_str());
-	/*SDL_Surface* sprite_surface = SDL_CreateRGBSurface(
-		bmp_surface->flags,
-		width, height,
-		32,
-		0, 0, 0, 0
-	);
-	SDL_BlitSurface(bmp_surface, &source_rect_, sprite_surface, NULL);
-	SDL_FreeSurface(bmp_surface);*/
-	texture_ = graphics.CreateTextureFromSurface(sprite_surface);  // TODO: truncate texture so that it only contains the sprite, not the whole BMP image
-	SDL_FreeSurface(sprite_surface);
+	width_ = sprite_surface->w;
+	height_ = sprite_surface->h;
+	// Alternative class construction and structure
+	// Would create and store a new, smaller surface that is a copy of desired section of the source image ("sub-image").
+	// PROS:
+	// - Eliminates need for source_x and source_y when later drawing (the whole surface is the intended sprite).
+	// - Reduces amount of data stored per sprite (texture is smaller size compared to source texture).
+	// CONS:
+	// - Cannot draw part of the sprite, as source_rect.x and source_rect.y will always be 0 (to draw the entire texture).
+	// - Multiple Sprites will be needed if many sub-images of a single sprite sheet image need to be drawn, one for each sub-image,
+	//   ultimately negating the per-sprite smaller size benefit.
+	// - Breaks up a single sprite sheet/tileset image into many individual objects that need to be managed.
+	/*
+		SDL_Surface* sprite_surface = SDL_CreateRGBSurface(
+			bmp_surface->flags,
+			width, height,
+			32,
+			0, 0, 0, 0
+		);
+		SDL_BlitSurface(bmp_surface, &source_rect_, sprite_surface, NULL);
+		source_rect_.x = 0;
+		source_rect_.y = 0;
+		SDL_FreeSurface(bmp_surface);
+	*/
+	// Set transparency based on RGB of given pixel
+	if (alpha_x >= 0 && alpha_y >= 0) {
+		Uint8 r = 0x00, g = 0x00, b = 0x00;
+		Uint32 alpha_pixel = graphics.GetSurfacePixel(sprite_surface, alpha_x, alpha_y);  // Get RGB values of first pixel
+		SDL_GetRGB(alpha_pixel, sprite_surface->format, &r, &g, &b);
+		color_key_ = SDL_MapRGB(sprite_surface->format, r, g, b);
+		SDL_SetColorKey(sprite_surface, SDL_TRUE, color_key_);
+	} else {
+		color_key_ = 0x00000000;
+	}
+	texture_ = graphics.CreateTextureFromSurface(sprite_surface);
+	SDL_FreeSurface(sprite_surface);  // perhaps store surface too if we later want to adjust the texture?
 }
 
 Sprite::~Sprite() {
 	SDL_DestroyTexture(texture_);
 }
 
+SDL_Rect Sprite::GetRect() {
+	return source_rect_;
+}
+
+void Sprite::SetRect(SDL_Rect& rect) {
+	source_rect_.x = rect.x;
+	source_rect_.y = rect.y;
+	source_rect_.w = rect.w;
+	source_rect_.h = rect.h;
+}
+
+void Sprite::SetRect(int rect_x, int rect_y, int rect_w, int rect_h) {
+	source_rect_.x = rect_x;
+	source_rect_.y = rect_y;
+	source_rect_.w = rect_w;
+	source_rect_.h = rect_h;
+}
+
 int Sprite::Draw(Graphics& graphics, int pos_x, int pos_y) {
 	SDL_Rect dest_rect = { pos_x, pos_y, source_rect_.w, source_rect_.h };
 	return graphics.BlitTexture(texture_, &source_rect_, &dest_rect);
+}
+
+int Sprite::Draw(Graphics& graphics, int pos_x, int pos_y, SDL_Rect alt_source_rect) {
+	SDL_Rect dest_rect = { pos_x, pos_y, alt_source_rect.w, alt_source_rect.h };
+	return graphics.BlitTexture(texture_, &alt_source_rect, &dest_rect);
 }
--- src/sprite.h
+++ src/sprite.h
@@ -4,14 +4,37 @@
 #include <string>
 #include "graphics.h"
 
+/// <summary>
+/// Stores a sprite sheet image and can draw a rectangular section of it.  Stores a sprite sheet(or any image really) as a SDL_Texture.
+/// Contains a gettable / settable SDL_Rect that specifies what part of theSDL_Texture to draw when Draw() is called.
+/// </summary>
 class Sprite {
 public:
-	Sprite(Graphics& graphics, const std::string& file_path, int source_x, int source_y, int width, int height);
+	/// <summary>
+	/// Constructs a Sprite class.
+	/// </summary>
+	/// <param name="graphics">Graphics rendering context to use.</param>
+	/// <param name="file_path">File path to the BMP image to load for the Sprite.</param>
+	/// <param name="alpha_x">X-coordinate in pixels of the pixel of the loaded image whose RGB value to treat as transparent.  Defaults to -1 (no transparency).</param>
+	/// <param name="alpha_y">Y-coordinate in pixels of the pixel of the loaded image whose RGB value to treat as transparent.  Defaults to -1 (no transparency).</param>
+	/// <param name="source_x">Initial x-coordinate of the rect used for rendering the sprite.  Defaults to -1 (will not render).</param>
+	/// <param name="source_y">Initial y-coordinate of the rect used for rendering the sprite.  Defaults to -1 (will not render).</param>
+	/// <param name="source_w">Initial width of the rect used for rendering the sprite.  Defaults to -1 (will not render).</param>
+	/// <param name="source_h">Initial height of the rect used for rendering the sprite.  Defaults to -1 (will not render).</param>
+	/// <returns></returns>
+	Sprite(Graphics& graphics, const std::string& file_path, int alpha_x = -1, int alpha_y = -1, int source_x = -1, int source_y = -1, int source_w = -1, int source_h = -1);
 	~Sprite();
 
+	SDL_Rect GetRect();
+	void SetRect(SDL_Rect& rect);
+	void SetRect(int rect_x, int rect_y, int rect_w, int rect_h);
+
 	int Draw(Graphics& graphics, int pos_x, int pos_y);
+	int Draw(Graphics& graphics, int pos_x, int pos_y, SDL_Rect alt_source_rect);  // Does NOT modify source_rect_ member
 
 private:
 	SDL_Texture* texture_;
 	SDL_Rect source_rect_;
+	int width_, height_;
+	Uint32 color_key_;
 };
