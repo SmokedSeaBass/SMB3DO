9dd08a76879719f08a43c4841a9a745bb1764e80
Sebastien Blazej <sebik.blazej@gmail.com>
2020-05-30 17:22:24 -0400

    Preliminary copyright statement and legal disclaimer.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,22 @@
 #include <stdio.h>
-#include <SDL.h>
 #include "game.h"
+
+/*
+  Super Mario Bros. 3-Do
+  Copyright (C) 2019-2020 Sebastien Blazej <smokedseabass@gmail.com>
+
+  This is a non-profit fangame made out of passion for Nintendo's classic NES game,
+  Super Mario Bros. 3.  This fangame is NOT affiliated with or endorsed by Nintendo.
+  I do NOT claim any assets which are trademarked by Nintendo or their respective owners
+  as my own, including but not limited to: the Super Mario trademark and the artwork
+  used in this fangame.
+
+  If you like this game, please consider supporting Nintendo and buying the original game.
+*/
 
 int main(int argc, char* args[]) {
 	printf("Initializng game...\n");
 	Game smb3do;
-	if (smb3do.Initialize() < 0) {
-		printf("ERROR: Failed to initialize game\n");
-		return -1;
-	}
 
 	printf("Starting main game loop...\n");
 	if (smb3do.Run() < 0) {
@@ -17,7 +25,6 @@ int main(int argc, char* args[]) {
 	}
 
 	printf("Closing game...\n");
-	smb3do.Close();
 
 	return 0;
 }
