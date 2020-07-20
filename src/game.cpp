c4b0c9a4b699c3aaaff3f8d0aa55ad0881bd7b65
Sebastien Blazej <smokedseabass@gmail.com>
2020-07-20 16:09:18 -0400

    Error handling and frame clock

--- src/game.cpp
+++ src/game.cpp
@@ -1,27 +1,27 @@
 #include "game.h"
 
+#include <chrono>
 #include "constants.h"
 #include "tilemap.h"
 #include "sprite.h"
 #include "player.h"
 #include "input.h"
+#include "error.h"
 
-Game::Game() {
-}
+Game::Game() { }
 
-Game::~Game() {
-}
+Game::~Game() { }
 
 int Game::Run() {
 	Graphics graphics;
 	if (graphics.Initialize() < 0) {
-		printf("ERROR: Grpahics could not initialize: %s\n", SDL_GetError());
+		Error::PrintError(std::runtime_error("Graphics could not initialize"));
 		return -1;
 	}
 	Input input;
 	bool quitGame = false;
 	SDL_Event event;
-	const Uint8* keyboard_state = SDL_GetKeyboardState(NULL);
+	const Uint8* keyboard_state = SDL_GetKeyboardState(nullptr);
 	
 	// Testing code
 	// Mario
@@ -33,17 +33,19 @@ int Game::Run() {
 	Tile air = Tile(0x00, &tileset_debug, { 0, 0, 16, 16 }, "air");
 	Tile wood = Tile(0x01, &tileset_debug, { 0, 16, 16, 16 }, "wood");
 	Tile* background_tiles[30] = {
-		nullptr, &air, & air, &air, nullptr,
-		&air, &wood, & air, &wood, &air,
-		&air, &air, & air, &air, &air,
-		&wood, &air, & air, &air, &wood,
-		&air, &wood, & wood, &wood, &air,
+		nullptr, &air, &air, &air, nullptr,
+		&air, &wood, &air, &wood, &air,
+		&air, &air, &air, &air, &air,
+		&wood, &air, &air, &air, &wood,
+		&air, &wood, &wood, &wood, &air,
 		nullptr, &air, & air, &air, nullptr
 	};
 	Tilemap background_tilemap = Tilemap(background_tiles, 5, 6);
 	
 	// Main game loop
 	while (!quitGame) {
+		const std::chrono::high_resolution_clock::time_point tick_start = std::chrono::high_resolution_clock::now();
+
 		/* Get Input */
 		// Handle events
 		while (SDL_PollEvent(&event) != 0) {
@@ -58,16 +60,29 @@ int Game::Run() {
 		/* Update */
 		mario.Update(input);
 
+
 		/* Draw */
+		graphics.SetViewport(Graphics::ViewportRects::screen_main_rect);
 		// Fill the NES 'screen' with blue color
 		graphics.BlitColoredRect(nullptr, 0x10, 0x10, 0x40, 0xFF);
 		// Draw tilemap
 		background_tilemap.Draw(graphics, 16, 16);
 		// Draw in the player
 		mario.Draw(graphics);
-
 		// Flip to screen
 		graphics.FlipRenderer();
+
+		// Frames-per-second management
+		const std::chrono::high_resolution_clock::time_point tick_end = std::chrono::high_resolution_clock::now();
+		const double tick_duration = std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(tick_end - tick_start).count();
+		double tick_wait = 0;
+		if (FPS_LIMIT > 0) tick_wait = 1000.0 / FPS_LIMIT - tick_duration;
+		if (tick_wait >= 0) SDL_Delay((Uint32)tick_wait);
+		// Add to window's title
+		char buff[48];
+		snprintf(buff, sizeof(buff), "%.0f/%.2f fps (%.4f/%.4f ms)", 1000.0 / (tick_duration + tick_wait), 1000.0 / tick_duration, tick_duration + tick_wait, tick_duration);
+		std::string subtitle(buff);
+		graphics.WindowSetTitle(subtitle);
 	}
 	return 0;
 }
