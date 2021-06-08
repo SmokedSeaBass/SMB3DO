4ff2324772572e3026cab00b3bc7b1e1bc364cec
SeaBass <smokedseabass@gmail.com>
2021-06-08 13:17:45 -0400

    Delete wood.tsx

--- assets/tilesets/wood.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="wood" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="1533" columns="73">
- <image source="wood.png" trans="7fff8e" width="1242" height="358"/>
-</tileset>
