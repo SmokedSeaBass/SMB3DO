b727d3fadd5be3e3930cf088af8aac5a9b2b7bd2
SmokedSeaBass <smokedseabass@gmail.com>
2021-06-21 20:54:18 -0400

    Allow for const char[] and string literal messages

--- src/error.cpp
+++ src/error.cpp
@@ -4,18 +4,18 @@
 #include <iostream>
 #include "constants.h"
 
-void Error::PrintError(std::string& message) {
+void Error::PrintError(const std::string& message) {
 	std::cout << ANSI_COLOR_RED << "ERROR" << ANSI_COLOR_RESET ": " << message << std::endl;
 }
 
-void Error::PrintError(std::exception exception) {
+void Error::PrintError(const std::exception exception) {
 	std::cout << ANSI_COLOR_RED << "ERROR" << ANSI_COLOR_RESET ": " << exception.what() << std::endl;
 }
 
-void Error::PrintWarning(std::string& message) {
+void Error::PrintWarning(const std::string& message) {
 	std::cout << ANSI_COLOR_YELLOW << "WARNING" << ANSI_COLOR_RESET ": " << message << std::endl;
 }
 
-void Error::PrintDebug(std::string& message) {
+void Error::PrintDebug(const std::string& message) {
 	if (META_DEBUG) std::cout << ANSI_COLOR_CYAN << "DEBUG" << ANSI_COLOR_RESET ": " << message << std::endl;
 }
--- src/error.h
+++ src/error.h
@@ -5,8 +5,8 @@
 
 struct Error {
 public:
-	static void PrintError(std::string& message);
-	static void PrintError(std::exception exception);
-	static void PrintWarning(std::string& message);
-	static void PrintDebug(std::string& message);
+	static void PrintError(const std::string& message);
+	static void PrintError(const std::exception exception);
+	static void PrintWarning(const std::string& message);
+	static void PrintDebug(const std::string& message);
 };
