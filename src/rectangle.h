30aaf0f30fdff62172bee5c1a6776bbfce52a3b1
SmokedSeaBass <smokedseabass@gmail.com>
2021-07-16 13:59:12 -0400

    Allow Rectangles to be added together

--- src/rectangle.h
+++ src/rectangle.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <SDL_rect.h>
 
 class Rectangle {
 public:
@@ -8,12 +9,21 @@ public:
 	Rectangle(double x, double y, double w, double h) :
 		x(x), y(y), w(w), h(h) {}
 	Rectangle(SDL_Rect sdl_rect) :
-		x(sdl_rect.x), y(sdl_rect.y), w(sdl_rect.w), h(sdl_rect.h) {}
+		x(static_cast<double>(sdl_rect.x)), y(static_cast<double>(sdl_rect.y)), w(static_cast<double>(sdl_rect.w)), h(static_cast<double>(sdl_rect.h)) {}
 
 	double Left() const { return x; }
 	double Right() const { return x + w; }
 	double Top() const { return y; }
 	double Bottom() const { return y + h; }
+
+	Rectangle operator+(const Rectangle& other) const {
+		return Rectangle(
+			this->x + other.x,
+			this->y + other.y,
+			this->w + other.w,
+			this->h + other.h
+		);
+	}
 
 	double x, y, w, h;
 };
