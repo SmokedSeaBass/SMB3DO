928728c263cb0b587e4c9391293cd058cf3f5342
SmokedSeaBass <smokedseabass@gmail.com>
2021-07-19 11:54:37 -0400

    Update debug assets
    * New tilemap (test2.tmx)
    * Two new tilesets (Standard and Wood)
    * Add NES palette reference

---
+++ assets/tilesets/standard.tsx
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="standard" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="standard.bmp" trans="e800dc" width="256" height="256"/>
+ <tile id="32">
+  <animation>
+   <frame tileid="32" duration="125"/>
+   <frame tileid="33" duration="125"/>
+   <frame tileid="34" duration="125"/>
+   <frame tileid="35" duration="125"/>
+  </animation>
+ </tile>
+ <tile id="37">
+  <animation>
+   <frame tileid="37" duration="125"/>
+   <frame tileid="38" duration="125"/>
+   <frame tileid="39" duration="125"/>
+   <frame tileid="40" duration="125"/>
+  </animation>
+ </tile>
+ <tile id="44">
+  <animation>
+   <frame tileid="44" duration="125"/>
+   <frame tileid="45" duration="125"/>
+   <frame tileid="46" duration="125"/>
+   <frame tileid="47" duration="125"/>
+  </animation>
+ </tile>
+ <tile id="48">
+  <animation>
+   <frame tileid="48" duration="125"/>
+   <frame tileid="49" duration="125"/>
+   <frame tileid="50" duration="125"/>
+   <frame tileid="51" duration="125"/>
+  </animation>
+ </tile>
+ <tile id="53">
+  <animation>
+   <frame tileid="53" duration="125"/>
+   <frame tileid="54" duration="125"/>
+   <frame tileid="55" duration="125"/>
+   <frame tileid="56" duration="125"/>
+  </animation>
+ </tile>
+ <tile id="60">
+  <animation>
+   <frame tileid="60" duration="125"/>
+   <frame tileid="61" duration="125"/>
+   <frame tileid="62" duration="125"/>
+   <frame tileid="63" duration="125"/>
+  </animation>
+ </tile>
+</tileset>
