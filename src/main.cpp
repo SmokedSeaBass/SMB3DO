9870a8d644e1de5e7c9bb90c100ea01c03ecb911
SmokedSeaBass <smokedseabass@tutanota.com>
2023-07-26 11:32:36 -0400

    Update licensing and copyright

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,29 @@
 #include "logger.h"
 
 /*
-  Super Mario Bros. 3-Do
-  Copyright (C) 2019-2022 Sebastien Blazej <smokedseabass@tutanota.com>
+	Super Mario Bros. 3-Do
+	Copyright (C) 2019-2023 Sebastien Blazej <smokedseabass@tutanota.com>
 
-  This is a non-profit fan-game made out of passion for Nintendo's classic NES game,
-  Super Mario Bros. 3.  This fangame is NOT affiliated with or endorsed by Nintendo in any way.
-  I do NOT claim any assets which are trademarked by Nintendo or their respective owners
-  as my own, including but not limited to: the Super Mario trademark and the artwork
-  used in this fangame.
+	This is a non-profit fan-game created out of passion for Nintendo's classic NES game,
+	Super Mario Bros. 3.  This fangame is NOT affiliated with or endorsed by Nintendo in
+	any way. I do NOT claim any assets which are trademarked by Nintendo or their
+	respective owners as my own, including but not limited to: the Super Mario trademark,
+	the original game assets, fan artwork, or reference materials.
 
-  If you like this game, please consider supporting Nintendo and buying the original game.
+	If you like this game, please consider supporting Nintendo and buying the original game.
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 int main(int argc, char* args[]) {
